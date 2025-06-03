5d95d4b020b8403b11b070d1493fe7bc01f474e9
carn181 <carn181@proton.me>
2025-06-03 17:56:17 -0400

    Initial Barebones Commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module faustlsp
+
+go 1.24.3
---
+++ logging/log.go
@@ -0,0 +1,36 @@
+package logging
+
+import (
+	"log"
+	"os"
+	"runtime"
+)
+
+// logPath defines the default log file path based on OS.
+var logPath string
+
+// Logger is the global logger instance.
+var Logger *log.Logger
+
+// Init initializes the logger with a file output.
+func Init() {
+	// TODO: Add proper default log file path for windows
+	// TODO: Add option to take log file path from user
+	// Determine the log file path based on the operating system.
+	switch runtime.GOOS {
+	case "windows":
+		logPath = "log.txt"
+	case "linux", "darwin", "freebsd", "openbsd", "netbsd", "plan9":
+		logPath = "/tmp/log.txt"
+	default:
+		logPath = "log.txt"
+	}
+
+	// Open the log file.  Create it if it doesn't exist.
+	f, err := os.OpenFile(logPath, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0644)
+	if err != nil {
+		panic(err)
+	}
+	// Initialize the logger to write to the file, without flags or prefixes.
+	Logger = log.New(f, "golsp: ", log.Ltime)
+}
